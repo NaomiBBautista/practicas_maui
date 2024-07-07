2b213276f876560934b8290d45085418df2574bf
Naomi Bautista <120536638+NaomiBBautista@users.noreply.github.com>
2024-07-07 17:42:55 -0600

    add files to new project

---
+++ TDMPW_2P_PR03/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace TDMPW_2P_PR03;
+
+public partial class MainPage : TabbedPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnSliderChange1(object sender, EventArgs e)
+	{
+		txtH1.Text = " " + sldH1.Value.ToString("N0") + " ";
+	}
+	private void OnSliderChange2(object sender, EventArgs e)
+	{
+		txtH2.Text = " " + sldH2.Value.ToString("N0") + " ";
+	}
+	private void OnSliderChange3(object sender, EventArgs e)
+	{
+		txtH3.Text = " " + sldH3.Value.ToString("N0") + " ";
+	}
+
+	private void NameEntry_TextChanged(object sender, TextChangedEventArgs e)
+	{
+		txtNombre.Text = $" {e.NewTextValue} ";
+	}
+
+	private void HobbieTextChanged(object sender, TextChangedEventArgs e)
+	{
+		txtHobbie.Text = $" {e.NewTextValue} ";
+	}
+
+	private void Switch_Toggled(object sender, ToggledEventArgs e)
+	{
+		sldPuntuar.IsEnabled = mySwitch.IsToggled;
+	}
+
+	private void OnSlider(object sender, EventArgs e)
+	{
+		txtPuntuar.Text = " " + sldPuntuar.Value.ToString("N0") + " ";
+		stpCantidad.Value = sldPuntuar.Value; 
+	}
+
+	private void OnStepper(object sender, EventArgs e)
+	{
+		txtPuntuar.Text = " " + stpCantidad.Value.ToString("N0") + " ";
+		sldPuntuar.Value = stpCantidad.Value; 
+	}
+}
+
---
+++ TDMPW_2P_PR03/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace TDMPW_2P_PR03;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("Hello Valentina.ttf", "Valentina");
+				fonts.AddFont("MittaSweety.ttf", "Mitta");
+				fonts.AddFont("TYPO_COMICS_bold_demo.otf", "ComicBold");
+				fonts.AddFont("TYPO_COMICS_demo.otf", "Comic");
+				fonts.AddFont("TYPO_COMICS_Light_demo.otf", "ComicLight");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
