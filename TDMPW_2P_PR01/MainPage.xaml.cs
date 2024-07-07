8c0e9b373bc91756a715df1525bf86224c4b60bf
Naomi Bautista <120536638+NaomiBBautista@users.noreply.github.com>
2024-07-07 16:15:56 -0600

    add new files

---
+++ TDMPW_2P_PR01/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace TDMPW_2P_PR01;
+
+public partial class MainPage : TabbedPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void meGusta(object sender, EventArgs e)
+	{
+		this.txtOpinion.Text = "Me encantó este personaje :)";
+	}
+
+	private void noGusta(object sender, EventArgs e)
+	{
+		this.txtOpinion.Text = "No me gustó este personaje :(";
+	}
+	private void meGusta1(object sender, EventArgs e)
+	{
+		this.txtOpinion1.Text = "Me encantó este personaje :)";
+	}
+
+	private void noGusta1(object sender, EventArgs e)
+	{
+		this.txtOpinion1.Text = "No me gustó este personaje :(";
+	}
+}
+
